c972afc3861a81c6bf1231c900877a8900c3eda8
souiai <merge.tool>
2022-09-12 12:46:58 +0200

    project_7_OC

---
+++ bruteforce.py
@@ -0,0 +1,34 @@
+import csv
+
+som = 0
+list_action = []
+capacity = 500
+with open('data_base/dataset3_Python+P7.csv', newline='') as file:
+    data = csv.reader(file)
+    for action in data:
+        if action[1] != 'price' and action[2] != 'profit':
+            action[1] = int(action[1])
+            action[2] = float(action[2])
+            list_action.append(list(action))
+
+
+# Solution force brute - Recherche de toutes les solutions
+def sacados_force_brute(capacity_max, actions, elements_selection=[]):
+    if actions:
+        val1, lst_val = sacados_force_brute(capacity_max, actions[1:], elements_selection)
+        val = actions[0]
+        if val[1] <= capacity_max:
+            val2, lst_val = sacados_force_brute(capacity_max - val[1], actions[1:], elements_selection + [val])
+            if val1 < val2:
+                return val2, lst_val
+
+        return val1, lst_val
+    else:
+        return sum([i[2] for i in elements_selection]), elements_selection
+
+
+results = sacados_force_brute(capacity, list_action)
+for profit in results[1]:
+    print(profit)
+    som += profit[2]
+print(f"Bénéfice :{results[0]}")
---
+++ optimized.py
@@ -0,0 +1,56 @@
+import csv
+import sys
+
+sys.setrecursionlimit(1050)
+som = 0
+list_action = []
+capacity = 500
+with open('data_base/dataset3_Python+P7.csv', newline='') as file:
+    data = csv.reader(file)
+    for action in data:
+        if action[1] != 'price' and action[2] != 'profit':
+            """action[1] = int(float(action[1]) * 100)
+            action[2] = int(float(action[2]) * 100)"""
+            action[1] = int(float(action[1]))
+            action[2] = int(float(action[2])*100)
+            list_action.append(list(action))
+
+"""list_action = sorted(
+    list_action, key=lambda x: x[1], reverse=False)"""
+
+"""for line in list_action:
+    print(line)"""
+
+
+# Solution optimale - programmation dynamique
+def sacADos_dynamique(capacity, list_action):
+    matrice = [[0 for x in range(capacity + 1)] for x in range(len(list_action) + 1)]
+
+    for i in range(1, len(list_action) + 1):
+        for w in range(1, capacity + 1):
+            if list_action[i-1][1] <= w:
+                matrice[i][w] = max(list_action[i-1][2] + matrice[i-1][w-list_action[i-1][1]], matrice[i-1][w])
+            else:
+                matrice[i][w] = matrice[i-1][w]
+
+    # Retrouver les éléments en fonction de la somme
+    w = capacity
+    n = len(list_action)
+    elements_selection = []
+
+    while w >= 0 and n >= 0:
+        e = list_action[n-1]
+        if matrice[n][w] == matrice[n-1][w-e[1]] + e[2]:
+            elements_selection.append(e)
+            w -= e[1]
+
+        n -= 1
+
+    return matrice[-1][-1], elements_selection
+
+
+results = sacADos_dynamique(capacity, list_action)
+for profit in results[1]:
+    print(profit)
+    som += profit[2]
+print(f"Bénéfice :{results[0]}")
